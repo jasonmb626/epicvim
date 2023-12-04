b8da24abe07c733292ed152dfdb073d073cd8f10
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-04 08:01:51 -0600

    Fix keymaps for telescope undo, AdvancedGitSearch

--- lua/epicvim/plugins/telescope.lua
+++ lua/epicvim/plugins/telescope.lua
@@ -77,20 +77,7 @@ return {
 				},
 			},
 			extensions = {
-				undo = {
-					use_delta = true,
-					use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
-					side_by_side = false,
-					diff_context_lines = vim.o.scrolloff,
-					entry_format = "state #$ID, $STAT, $TIME",
-					mappings = {
-						i = {
-							["<C-cr>"] = require("telescope-undo.actions").yank_additions,
-							["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
-							["<cr>"] = require("telescope-undo.actions").restore,
-						},
-					},
-				},
+				undo = {},
 			},
 		})
 
