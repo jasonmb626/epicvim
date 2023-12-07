8eb60e9a22de339941c9c3eb551cb8943776b131
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-07 06:56:38 -0600

    Fix configure ==> config

--- lua/epicvim/plugins/gitsigns.lua
+++ lua/epicvim/plugins/gitsigns.lua
@@ -1,7 +1,7 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = {},
-	configure = function()
+	config = function()
 		require("gitsigns").setup({
 			signs = {
 				add = { text = "│" },
@@ -43,17 +43,48 @@ return {
 			yadm = {
 				enable = false,
 			},
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true })
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true })
+
+				vim.keymap.set("n", "]h", require("gitsigns").next_hunk, { desc = "Next Hunk" })
+				vim.keymap.set("n", "[h", require("gitsigns").prev_hunk, { desc = "Prev Hunk" })
+				vim.keymap.set("n", "<leader>vB", require("gitsigns").blame_line, { desc = "Blame" })
+				vim.keymap.set("n", "<leader>vd", ":Gitsigns diffthis HEAD<cr>", { desc = "Git Diff (against head)" })
+				vim.keymap.set("n", "<leader>vG", ":AdvancedGitSearch<cr>", { desc = "Advanced Git Search" })
+				vim.keymap.set("n", "<leader>vj", require("gitsigns").next_hunk, { desc = "Next Hunk" })
+				vim.keymap.set("n", "<leader>vk", require("gitsigns").prev_hunk, { desc = "Prev Hunk" })
+				vim.keymap.set("n", "<leader>vp", require("gitsigns").preview_hunk, { desc = "Preview Hunk" })
+				vim.keymap.set("n", "<leader>vr", require("gitsigns").reset_hunk, { desc = "Reset Hunk" })
+				vim.keymap.set("n", "<leader>vs", require("gitsigns").stage_hunk, { desc = "Stage Hunk" })
+				vim.keymap.set("n", "<leader>vu", require("gitsigns").undo_stage_hunk, { desc = "Undo Stage Hunk" })
+			end,
 		})
-		vim.keymap.set("n", "]h", require("gitsigns").next_hunk, { desc = "Next Hunk" })
-		vim.keymap.set("n", "[h", require("gitsigns").prev_hunk, { desc = "Prev Hunk" })
-		vim.keymap.set("n", "<leader>vB", require("gitsigns").blame_line, { desc = "Blame" })
-		vim.keymap.set("n", "<leader>vd", ":Gitsigns diffthis HEAD<cr>", { desc = "Git Diff (against head)" })
-		vim.keymap.set("n", "<leader>vG", ":AdvancedGitSearch<cr>", { desc = "Advanced Git Search" })
-		vim.keymap.set("n", "<leader>vj", require("gitsigns").next_hunk, { desc = "Next Hunk" })
-		vim.keymap.set("n", "<leader>vk", require("gitsigns").prev_hunk, { desc = "Prev Hunk" })
-		vim.keymap.set("n", "<leader>vp", require("gitsigns").preview_hunk, { desc = "Preview Hunk" })
-		vim.keymap.set("n", "<leader>vr", require("gitsigns").reset_hunk, { desc = "Reset Hunk" })
-		vim.keymap.set("n", "<leader>vs", require("gitsigns").stage_hunk, { desc = "Stage Hunk" })
-		vim.keymap.set("n", "<leader>vu", require("gitsigns").undo_stage_hunk, { desc = "Undo Stage Hunk" })
 	end,
 }
