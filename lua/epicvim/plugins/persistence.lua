6a9afe4143178e021254fcf9f8604474ee77e009
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-08 14:11:13 +0000

    Add explicit setup(opts). Didn't help didn't hurt.

--- lua/epicvim/plugins/persistence.lua
+++ lua/epicvim/plugins/persistence.lua
@@ -2,7 +2,8 @@ return {
 	"folke/persistence.nvim",
 	event = "BufReadPre",
 	opts = { options = vim.opt.sessionoptions:get() },
-	config = function()
+	config = function(_, opts)
+		require("persistence").setup(opts)
 		vim.keymap.set("n", "<leader>qd", require("persistence").stop, { desc = "Don't Save Current Session" })
 		vim.keymap.set("n", "<leader>ql", function()
 			require("persistence").load({ last = true })
