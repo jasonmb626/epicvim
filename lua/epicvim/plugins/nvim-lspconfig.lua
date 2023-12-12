cdc4f0aecbaa6210d933249638af262ab499f828
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-12 05:18:17 -0600

    Add jump to prev/next diagnostic to code action menu

--- lua/epicvim/plugins/nvim-lspconfig.lua
+++ lua/epicvim/plugins/nvim-lspconfig.lua
@@ -48,9 +48,11 @@ return {
 
 			opts.desc = "Go to prev diagnostic"
 			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+			keymap.set("n", "<leader>cp", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
 			opts.desc = "Go to next diagnostic"
 			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+			keymap.set("n", "<leader>cn", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
 			opts.desc = "Show documentation for what is under cursor"
 			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
