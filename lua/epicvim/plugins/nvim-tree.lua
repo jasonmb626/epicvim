c60857fa8d65e0e06cbe75f1eb2322f2b74b3a77
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-12 12:31:26 -0600

    Add focus file explorer keymap

--- lua/epicvim/plugins/nvim-tree.lua
+++ lua/epicvim/plugins/nvim-tree.lua
@@ -53,5 +53,6 @@ return {
 		vim.keymap.set("n", "<leader>fc", ":NvimTreeCollapse<cr>", { desc = "Collapse file explorer" })
 		vim.keymap.set("n", "<leader>fe", ":NvimTreeToggle<cr>", { desc = "Toggle file explorer" })
 		vim.keymap.set("n", "<leader>ft", ":NvimTreeFindFile<cr>", { desc = "File explorer on this file" })
+		vim.keymap.set("n", "<leader>fu", ":NvimTreeFocus<cr>", { desc = "Focus file explorer" })
 	end,
 }
