378811dc6dd826ba058cbd231993d71283f14a5c
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-12 12:59:12 -0600

    Re-order for fewer files

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
-require("epicvim.config.options")
+require("epicvim.config.options") -- make sure mapleader and global plugin configs set first
 require("epicvim.setup_lazy")
-require("epicvim.config")
+require("epicvim.config.autocmds")
+require("epicvim.config.keymaps") -- make sure keymaps is last. We really do want these to override anything else
--- lua/epicvim/config/init.lua
+++
@@ -1,3 +0,0 @@
-require("epicvim.config.autocmds")
-require("epicvim.config.keymaps")
-require("epicvim.config.options")
