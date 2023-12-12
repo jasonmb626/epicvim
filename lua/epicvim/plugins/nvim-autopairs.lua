7b92eaf363b61a9cdd1158d359ad30893e3d3a2d
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-12 14:07:53 -0600

    Add section from docs that was missing

--- lua/epicvim/plugins/nvim-autopairs.lua
+++ lua/epicvim/plugins/nvim-autopairs.lua
@@ -7,6 +7,7 @@ return {
 	config = function()
 		-- import nvim-autopairs
 		local autopairs = require("nvim-autopairs")
+		local Rule = require("nvim-autopairs.rule")
 
 		-- configure autopairs
 		autopairs.setup({
@@ -16,6 +17,14 @@ return {
 				javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
 				java = false, -- don't check treesitter on java
 			},
+		})
+
+		local ts_conds = require("nvim-autopairs.ts-conds")
+
+		-- press % => %% only while inside a comment or string
+		autopairs.add_rules({
+			Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
+			Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
 		})
 
 		-- import nvim-autopairs completion functionality
