e21315c55b1d6b334dfdcd35d8e67bac39312297
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-12 07:17:46 -0600

    Change colors. Add recent projects, edit config entries

--- lua/epicvim/plugins/alpha-nvim.lua
+++ lua/epicvim/plugins/alpha-nvim.lua
@@ -1,14 +1,7 @@
 return {
-
-	{ "nvimdev/dashboard-nvim", enabled = false },
-	{ "echasnovski/mini.starter", enabled = false },
-	-- Dashboard. This runs when neovim starts, and is what displays
-	-- the "LAZYVIM" banner.
 	{
 		"goolord/alpha-nvim",
-		event = "VimEnter",
-		enabled = true,
-		init = false,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = function()
 			local dashboard = require("alpha.themes.dashboard")
 			--https://www.asciiart.eu/text-to-ascii-art
@@ -29,57 +22,39 @@ return {
 //////////////////////////////////////////////////////////////////////////////////
 ]]
 			dashboard.section.header.val = vim.split(logo, "\n")
-      -- stylua: ignore
-      dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
-        dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
-        dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
-        dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
-        dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-        dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
-        dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
-      }
+			dashboard.section.buttons.val = {
+				dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+				dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+				dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+				dashboard.button(
+					"p",
+					" " .. " Recent Projects",
+					"<cmd> lua require('telescope').extensions.project.project({})<cr>"
+				),
+				dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
+				dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+				dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+				dashboard.button(
+					"c",
+					" " .. " Configuration",
+					"<cmd>lua vim.api.nvim_command('edit ' .. vim.call('stdpath', 'config'))<cr>"
+				),
+				dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+			}
 			for _, button in ipairs(dashboard.section.buttons.val) do
-				button.opts.hl = "AlphaButtons"
-				button.opts.hl_shortcut = "AlphaShortcut"
+				button.opts.hl = "Question"
+				button.opts.hl_shortcut = "Question"
 			end
-			dashboard.section.header.opts.hl = "AlphaHeader"
-			dashboard.section.buttons.opts.hl = "AlphaButtons"
-			dashboard.section.footer.opts.hl = "AlphaFooter"
-			dashboard.opts.layout[1].val = 8
+			-- to get a list of available hl options type :hl
+			dashboard.section.header.opts.hl = "@text.title"
 			return dashboard
 		end,
 		config = function(_, dashboard)
-			-- close Lazy and re-open when the dashboard is ready
-			if vim.o.filetype == "lazy" then
-				vim.cmd.close()
-				vim.api.nvim_create_autocmd("User", {
-					once = true,
-					pattern = "AlphaReady",
-					callback = function()
-						require("lazy").show()
-					end,
-				})
-			end
-
 			require("alpha").setup(dashboard.opts)
-
-			vim.api.nvim_create_autocmd("User", {
-				once = true,
-				pattern = "LazyVimStarted",
-				callback = function()
-					local stats = require("lazy").stats()
-					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-					dashboard.section.footer.val = "⚡ Neovim loaded "
-						.. stats.loaded
-						.. "/"
-						.. stats.count
-						.. " plugins in "
-						.. ms
-						.. "ms"
-					pcall(vim.cmd.AlphaRedraw)
-				end,
-			})
+			-- Disable folding on alpha buffer
+			vim.cmd([[
+    autocmd FileType alpha setlocal nofoldenable
+]])
 		end,
 	},
 }
