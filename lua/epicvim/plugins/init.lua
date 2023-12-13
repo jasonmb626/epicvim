a5ab6903fea05c0763468fbc8f8ac24e8728b2b2
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-13 05:55:38 -0600

    Add (but don't enable) Neosolarized theme

--- lua/epicvim/plugins/init.lua
+++ lua/epicvim/plugins/init.lua
@@ -36,6 +36,14 @@ return {
 		end,
 	},
 	{
+		"Tsuzat/NeoSolarized.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			--vim.cmd [[ colorscheme NeoSolarized ]]
+		end,
+	},
+	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
 		opts = {},
