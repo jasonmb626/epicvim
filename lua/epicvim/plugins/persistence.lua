3bd4c3c789e24edb667b3a424d5184515e24b208
Jason Brunelle <jason@jasonbrunelle.com>
2023-12-15 14:51:13 -0600

    Formatting change

--- lua/epicvim/plugins/persistence.lua
+++ lua/epicvim/plugins/persistence.lua
@@ -1,13 +1,14 @@
-return {
-	"folke/persistence.nvim",
-	event = "BufReadPre",
-	opts = { options = vim.opt.sessionoptions:get() },
-	config = function(_, opts)
-		require("persistence").setup(opts)
-		vim.keymap.set("n", "<leader>qd", require("persistence").stop, { desc = "Don't Save Current Session" })
-		vim.keymap.set("n", "<leader>ql", function()
-			require("persistence").load({ last = true })
-		end, { desc = "Restore Last Session" })
-		vim.keymap.set("n", "<leader>qs", require("persistence").load, { desc = "Restore Session" })
-	end,
-}
+return {}
+-- return {
+-- 	"folke/persistence.nvim",
+-- 	event = "BufReadPre",
+-- 	opts = { options = vim.opt.sessionoptions:get() },
+-- 	config = function(_, opts)
+-- 		require("persistence").setup(opts)
+-- 		vim.keymap.set("n", "<leader>qd", require("persistence").stop, { desc = "Don't Save Current Session" })
+-- 		vim.keymap.set("n", "<leader>ql", function()
+-- 			require("persistence").load({ last = true })
+-- 		end, { desc = "Restore Last Session" })
+-- 		vim.keymap.set("n", "<leader>qs", require("persistence").load, { desc = "Restore Session" })
+-- 	end,
+-- }
