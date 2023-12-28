5cbc11657ee27d3a126c631a293d0e4b9c85a7fa
Jason Brunelle <jasonmb626@gmail.com>
2023-12-28 12:47:25 -0600

    Dynamically insert UID and GID when creating project structure.

--- if_docker/prep_for_neovim_docker.sh
+++ if_docker/prep_for_neovim_docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-cp ~/.config/nvim/if_docker/docker-compose.yml $PWD
+cat ~/.config/nvim/if_docker/docker-compose.yml| sed "s/%UID%/$(id -u)/" | sed "s/%GID%/$(id -g)/" > $PWD/docker-compose.yml
 cp ~/.config/nvim/if_docker/Dockerfile $PWD
 touch $PWD/requirements.txt
 touch $PWD/requirements-dev.txt
