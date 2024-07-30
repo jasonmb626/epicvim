160959198f8f788175f5355a55e3474c4678f6ad
Jason Brunelle <jasonmb626@gmail.com>
2024-07-30 07:50:50 -0500

    Switch to regexp branch, recommended by documentation and checkhealth

--- lua/epicvim/plugins/venv-selector.lua
+++ lua/epicvim/plugins/venv-selector.lua
@@ -1,16 +1,12 @@
 return {
-	"linux-cultist/venv-selector.nvim",
-	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-	opts = {
-		-- Your options go here
-		-- name = "venv",
-		-- auto_refresh = false
-	},
-	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-	keys = {
-		-- Keymap to open VenvSelector to pick a venv.
-		{ "<leader>ccs", "<cmd>VenvSelect<cr>" },
-		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-		{ "<leader>ccc", "<cmd>VenvSelectCached<cr>" },
-	},
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
+  keys = {
+    -- Keymap to open VenvSelector to pick a venv.
+    { "<leader>ccs", "<cmd>VenvSelect<cr>" },
+    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+    { "<leader>ccc", "<cmd>VenvSelectCached<cr>" },
+  },
 }
