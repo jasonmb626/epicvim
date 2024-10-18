c295969a018db86b6724c5865f4b7617b020cde0
Jason Brunelle <jasonmb626@gmail.com>
2024-10-18 08:20:04 -0500

    Don't assume directory of dockerfiles. Determine from path of this executable.

--- if_docker/prep_for_neovim_docker.sh
+++ if_docker/prep_for_neovim_docker.sh
@@ -5,14 +5,15 @@ if [[ ! -s ~/.ssh/docker_id_ed25519 ]]; then
   chown $(id -u):$(id -g) ~/.ssh/docker_id_ed25519
   chmod 600 ~/.ssh/docker_id_ed25519
 fi
-cp ~/.config/nvim/if_docker/Dockerfile $PWD
+basedir=$(dirname $0)
+cp $basedir/Dockerfile $PWD
 if [[ "$1" == "-R" ]]; then
-  cat ~/.config/nvim/if_docker/docker-compose.yml| sed "s/%UID%/0/" | sed "s/%GID%/0/" > $PWD/docker-compose.yml
+  cat $basedir/docker-compose.yml| sed "s/%UID%/0/" | sed "s/%GID%/0/" > $PWD/docker-compose.yml
   sed -i 's,/home/app/,/root/,g' $PWD/docker-compose.yml
   sed -i 's,/home/app/,/root/,g' $PWD/Dockerfile
   sed -i 's/app:app/root:root/g' $PWD/Dockerfile
 else
-  cat ~/.config/nvim/if_docker/docker-compose.yml| sed "s/%UID%/$(id -u)/" | sed "s/%GID%/$(id -g)/" > $PWD/docker-compose.yml
+  cat $basedir/docker-compose.yml| sed "s/%UID%/$(id -u)/" | sed "s/%GID%/$(id -g)/" > $PWD/docker-compose.yml
 fi
 touch $PWD/requirements.txt
 touch $PWD/requirements-dev.txt
